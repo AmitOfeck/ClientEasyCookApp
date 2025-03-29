8010fa8f2558fb6f0dfd86a4f7336c9b6e83d3ba
Amit Ofeck <Amitofeck1@gmail.com>
2025-03-29 12:53:44 +0300

    removed the bearer token + use api client in add dish to shopping list

--- src/services/shopping_list_service.ts
+++ src/services/shopping_list_service.ts
@@ -2,21 +2,40 @@ import apiClient from './api-client';
 import axios from 'axios';
 
 
+// const addDishesToShoppingList = (dishIds: string[], accessToken: string) => {
+//     const abortController = new AbortController();
+  
+//     const request = axios.post(
+//       'http://10.0.2.2:3000/shopping-list/add-dishes', // not throw client api
+//       { dishIds },
+//       {
+//         signal: abortController.signal,
+//         headers: {
+//           Authorization: accessToken, 
+//         },
+//       }
+//     );
+  
+//     return { request, abort: () => abortController.abort() };
+//   };
+
 const addDishesToShoppingList = (dishIds: string[], accessToken: string) => {
     const abortController = new AbortController();
   
-    const request = axios.post(
-      'http://10.0.2.2:3000/shopping-list/add-dishes', // not throw client api
+    const request = apiClient.post(
+      '/shopping-list/add-dishes',
       { dishIds },
       {
         signal: abortController.signal,
         headers: {
           Authorization: accessToken, 
+          'X-Skip-Auth': 'true', 
         },
       }
     );
   
     return { request, abort: () => abortController.abort() };
   };
+  
 
 export { addDishesToShoppingList };
